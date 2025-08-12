b764ebb9bb519de1a0e8129c1f6552de94e41fe7
Adewale Abdulmuiz <167482281+DevMuzee@users.noreply.github.com>
2025-08-12 15:16:26 +0100

    temp_data_warehouse
    Local data warehouse where the collected data is stored

--- e_commece_staging.sql
+++ e_commece_staging.sql
@@ -1,5 +1,6 @@
--- CREATE DATABASE e_commerce
-/*
+--creating a database
+CREATE DATABASE e_commerce
+
 ---creating the staging database
 CREATE TABLE online_retail(
     InvoiceNo INT,
@@ -16,19 +17,19 @@ CREATE TABLE online_retail(
 --altering the table column
 ALTER TABLE online_retail 
 ALTER COLUMN invoiceno TYPE TEXT USING invoiceno::TEXT;
-*/
 
----SELECT column_name, data_type
---FROM information_schema.columns
---WHERE table_name='online_retail';
+-- getting the column name and data types
+-SELECT column_name, data_type
+FROM information_schema.columns
+WHERE table_name='online_retail';
 
-/*
+--invoice no containing c
 SELECT customer_id, quantity, unitprice, stockcode, invoiceno
 FROM online_retail
 WHERE invoiceno ILIKE 'C%'
 LIMIT 3;
-*/
 
+--- getting customer_id not null
 SELECT *
 FROM online_retail
 WHERE customer_id IS NOT NULL
